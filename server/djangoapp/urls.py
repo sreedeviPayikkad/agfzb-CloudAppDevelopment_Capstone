540def742f3059abe5bf8bef0172e8bf77c0490c
sreedeviPayikkad <sreedevi.payikkad@gmail.com>
2022-02-16 20:20:20 -0800

    updated manifest.yml ans settings.py for cf route

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
 
     # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
