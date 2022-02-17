6a7dfa35affb2a015ded6a1e7ba19e67ce149846
sreedeviPayikkad <sreedevi.payikkad@gmail.com>
2022-02-16 19:36:29 -0800

    adding static files and changes for auth

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,18 +8,22 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for about view
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
+     path('registration/', views.registration_request, name='registration'),
 
     # path for login
+     path('login/', views.login_request, name='login'),
 
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+     path('logout/', views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
